4f029181f4a9acdeb9d4fe13dca3321a73a4e10e
Jason Reilly <jdr0887@gmail.com>
2017-04-06 09:07:02 -0400

    updating to reflect active workflow

--- gs-merge-vc-workflow/src/test/java/edu/unc/mapseq/workflow/WorkflowTest.java
+++ gs-merge-vc-workflow/src/test/java/edu/unc/mapseq/workflow/WorkflowTest.java
@@ -13,24 +13,14 @@ import org.renci.jlrm.condor.CondorJobEdge;
 import org.renci.jlrm.condor.ext.CondorDOTExporter;
 import org.renci.jlrm.condor.ext.CondorJobVertexNameProvider;
 
-import edu.unc.mapseq.module.sequencing.WriteVCFHeaderCLI;
-import edu.unc.mapseq.module.sequencing.bwa.BWAAlignCLI;
-import edu.unc.mapseq.module.sequencing.bwa.BWASAMPairedEndCLI;
-import edu.unc.mapseq.module.sequencing.fastqc.FastQCCLI;
-import edu.unc.mapseq.module.sequencing.filter.FilterVariantCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKApplyRecalibrationCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKCountCovariatesCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKDepthOfCoverageCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKFlagStatCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKIndelRealignerCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKRealignerTargetCreatorCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKTableRecalibrationCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKUnifiedGenotyperCLI;
-import edu.unc.mapseq.module.sequencing.gatk.GATKVariantRecalibratorCLI;
-import edu.unc.mapseq.module.sequencing.picard.PicardAddOrReplaceReadGroupsCLI;
-import edu.unc.mapseq.module.sequencing.picard.PicardFixMateCLI;
-import edu.unc.mapseq.module.sequencing.picard.PicardMarkDuplicatesCLI;
-import edu.unc.mapseq.module.sequencing.samtools.SAMToolsFlagstatCLI;
+import edu.unc.mapseq.module.core.RemoveCLI;
+import edu.unc.mapseq.module.core.ZipCLI;
+import edu.unc.mapseq.module.sequencing.freebayes.FreeBayesCLI;
+import edu.unc.mapseq.module.sequencing.gatk.GATKVariantAnnotatorCLI;
+import edu.unc.mapseq.module.sequencing.picard.PicardSortVCFCLI;
+import edu.unc.mapseq.module.sequencing.picard2.PicardCollectHsMetricsCLI;
+import edu.unc.mapseq.module.sequencing.picard2.PicardMergeSAMCLI;
+import edu.unc.mapseq.module.sequencing.samtools.SAMToolsDepthCLI;
 import edu.unc.mapseq.module.sequencing.samtools.SAMToolsIndexCLI;
 
 public class WorkflowTest {
@@ -43,134 +33,49 @@ public class WorkflowTest {
         int count = 0;
 
         // new job
-        CondorJob writeVCFHeaderJob = new CondorJobBuilder().name(String.format("%s_%d", WriteVCFHeaderCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(writeVCFHeaderJob);
-
-        // new job
-        CondorJob fastQCR1Job = new CondorJobBuilder().name(String.format("%s_%d", FastQCCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(fastQCR1Job);
-
-        // new job
-        CondorJob bwaAlignR1Job = new CondorJobBuilder().name(String.format("%s_%d", BWAAlignCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(bwaAlignR1Job);
-        graph.addEdge(fastQCR1Job, bwaAlignR1Job);
-
-        // new job
-        CondorJob fastQCR2Job = new CondorJobBuilder().name(String.format("%s_%d", FastQCCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(fastQCR2Job);
-
-        // new job
-        CondorJob bwaAlignR2Job = new CondorJobBuilder().name(String.format("%s_%d", BWAAlignCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(bwaAlignR2Job);
-        graph.addEdge(fastQCR2Job, bwaAlignR2Job);
-
-        // new job
-        CondorJob bwaSAMPairedEndJob = new CondorJobBuilder().name(String.format("%s_%d", BWASAMPairedEndCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(bwaSAMPairedEndJob);
-        graph.addEdge(bwaAlignR1Job, bwaSAMPairedEndJob);
-        graph.addEdge(bwaAlignR2Job, bwaSAMPairedEndJob);
-
-        // new job
-        CondorJob picardAddOrReplaceReadGroupsJob = new CondorJobBuilder()
-                .name(String.format("%s_%d", PicardAddOrReplaceReadGroupsCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(picardAddOrReplaceReadGroupsJob);
-        graph.addEdge(bwaSAMPairedEndJob, picardAddOrReplaceReadGroupsJob);
+        CondorJob mergeBAMFilesJob = new CondorJobBuilder().name(String.format("%s_%d", PicardMergeSAMCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(mergeBAMFilesJob);
 
         // new job
         CondorJob samtoolsIndexJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsIndexCLI.class.getSimpleName(), ++count)).build();
         graph.addVertex(samtoolsIndexJob);
-        graph.addEdge(picardAddOrReplaceReadGroupsJob, samtoolsIndexJob);
+        graph.addEdge(mergeBAMFilesJob, samtoolsIndexJob);
 
         // new job
-        CondorJob picardMarkDuplicatesJob = new CondorJobBuilder()
-                .name(String.format("%s_%d", PicardMarkDuplicatesCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(picardMarkDuplicatesJob);
-        graph.addEdge(samtoolsIndexJob, picardMarkDuplicatesJob);
+        CondorJob zipJob = new CondorJobBuilder().name(String.format("%s_%d", ZipCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(zipJob);
+        graph.addEdge(samtoolsIndexJob, zipJob);
 
         // new job
-        samtoolsIndexJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsIndexCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(samtoolsIndexJob);
-        graph.addEdge(picardMarkDuplicatesJob, samtoolsIndexJob);
+        CondorJob samtoolsDepthJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsDepthCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(samtoolsDepthJob);
+        graph.addEdge(samtoolsIndexJob, samtoolsDepthJob);
 
         // new job
-        CondorJob gatkRealignTargetCreatorJob = new CondorJobBuilder()
-                .name(String.format("%s_%d", GATKRealignerTargetCreatorCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(gatkRealignTargetCreatorJob);
-        graph.addEdge(samtoolsIndexJob, gatkRealignTargetCreatorJob);
+        CondorJob picardCollectHsMetricsJob = new CondorJobBuilder()
+                .name(String.format("%s_%d", PicardCollectHsMetricsCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(picardCollectHsMetricsJob);
+        graph.addEdge(samtoolsIndexJob, picardCollectHsMetricsJob);
 
         // new job
-        CondorJob gatkIndelRealignerJob = new CondorJobBuilder().name(String.format("%s_%d", GATKIndelRealignerCLI.class.getSimpleName(), ++count))
-                .build();
-        graph.addVertex(gatkIndelRealignerJob);
-        graph.addEdge(gatkRealignTargetCreatorJob, gatkIndelRealignerJob);
+        CondorJob freeBayesVCFJob = new CondorJobBuilder().name(String.format("%s_%d", FreeBayesCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(freeBayesVCFJob);
+        graph.addEdge(samtoolsIndexJob, freeBayesVCFJob);
 
         // new job
-        CondorJob picardFixMateJob = new CondorJobBuilder().name(String.format("%s_%d", PicardFixMateCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(picardFixMateJob);
-        graph.addEdge(gatkIndelRealignerJob, picardFixMateJob);
+        CondorJob picardSortVCFJob = new CondorJobBuilder().name(String.format("%s_%d", PicardSortVCFCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(picardSortVCFJob);
+        graph.addEdge(freeBayesVCFJob, picardSortVCFJob);
 
         // new job
-        samtoolsIndexJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsIndexCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(samtoolsIndexJob);
-        graph.addEdge(picardFixMateJob, samtoolsIndexJob);
+        CondorJob gatkVCFJob = new CondorJobBuilder().name(String.format("%s_%d", GATKVariantAnnotatorCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(gatkVCFJob);
+        graph.addEdge(picardSortVCFJob, gatkVCFJob);
 
         // new job
-        CondorJob gatkCountCovariatesJob = new CondorJobBuilder().name(String.format("%s_%d", GATKCountCovariatesCLI.class.getSimpleName(), ++count))
-                .build();
-        graph.addVertex(gatkCountCovariatesJob);
-        graph.addEdge(samtoolsIndexJob, gatkCountCovariatesJob);
-
-        // new job
-        CondorJob gatkTableRecalibrationJob = new CondorJobBuilder()
-                .name(String.format("%s_%d", GATKTableRecalibrationCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(gatkTableRecalibrationJob);
-        graph.addEdge(gatkCountCovariatesJob, gatkTableRecalibrationJob);
-
-        // new job
-        samtoolsIndexJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsIndexCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(samtoolsIndexJob);
-        graph.addEdge(gatkTableRecalibrationJob, samtoolsIndexJob);
-
-        // new job
-        CondorJob samtoolsFlagstatJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsFlagstatCLI.class.getSimpleName(), ++count))
-                .build();
-        graph.addVertex(samtoolsFlagstatJob);
-        graph.addEdge(samtoolsIndexJob, samtoolsFlagstatJob);
-
-        // new job
-        CondorJob gatkFlagstatJob = new CondorJobBuilder().name(String.format("%s_%d", GATKFlagStatCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(gatkFlagstatJob);
-        graph.addEdge(samtoolsIndexJob, gatkFlagstatJob);
-
-        // new job
-        CondorJob gatkDepthOfCoverageJob = new CondorJobBuilder().name(String.format("%s_%d", GATKDepthOfCoverageCLI.class.getSimpleName(), ++count))
-                .build();
-        graph.addVertex(gatkDepthOfCoverageJob);
-        graph.addEdge(samtoolsFlagstatJob, gatkDepthOfCoverageJob);
-        graph.addEdge(gatkFlagstatJob, gatkDepthOfCoverageJob);
-
-        // new job
-        CondorJob gatkUnifiedGenotyperJob = new CondorJobBuilder()
-                .name(String.format("%s_%d", GATKUnifiedGenotyperCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(gatkUnifiedGenotyperJob);
-        graph.addEdge(gatkDepthOfCoverageJob, gatkUnifiedGenotyperJob);
-
-        // new job
-        CondorJob filterVariant1Job = new CondorJobBuilder().name(String.format("%s_%d", FilterVariantCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(filterVariant1Job);
-        graph.addEdge(gatkUnifiedGenotyperJob, filterVariant1Job);
-
-        // new job
-        CondorJob gatkVariantRecalibratorJob = new CondorJobBuilder()
-                .name(String.format("%s_%d", GATKVariantRecalibratorCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(gatkVariantRecalibratorJob);
-        graph.addEdge(filterVariant1Job, gatkVariantRecalibratorJob);
-
-        // new job
-        CondorJob gatkApplyRecalibrationJob = new CondorJobBuilder()
-                .name(String.format("%s_%d", GATKApplyRecalibrationCLI.class.getSimpleName(), ++count)).build();
-        graph.addVertex(gatkApplyRecalibrationJob);
-        graph.addEdge(gatkVariantRecalibratorJob, gatkApplyRecalibrationJob);
+        CondorJob removeJob = new CondorJobBuilder().name(String.format("%s_%d", RemoveCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(removeJob);
+        graph.addEdge(gatkVCFJob, removeJob);
 
         CondorJobVertexNameProvider vnp = new CondorJobVertexNameProvider();
         CondorDOTExporter<CondorJob, CondorJobEdge> dotExporter = new CondorDOTExporter<CondorJob, CondorJobEdge>(vnp, vnp, null, null, null, null);
